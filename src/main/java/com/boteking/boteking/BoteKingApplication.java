e730f334598a8c049576518a1904a7834ab19d5c
Monteir0x <msmonteiro@id.uff.br>
2023-05-12 10:11:52 -0300

    [build] First Commit building an application for a PUB management.

---
+++ src/main/java/com/boteking/boteking/BoteKingApplication.java
@@ -0,0 +1,13 @@
+package com.boteking.boteking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoteKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoteKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boteking/boteking/BoteKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boteking.boteking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoteKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
